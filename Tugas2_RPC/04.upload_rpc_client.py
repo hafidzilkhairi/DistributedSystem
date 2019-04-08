ce9b2fa6166a8b84c20fe1c515716384d6c63feb
madityaar <m.adityarayhan@yahoo.co.id>
2019-04-08 19:34:19 +0700

    soal4

--- Tugas2_RPC/04.upload_rpc_client.py
+++ Tugas2_RPC/04.upload_rpc_client.py
@@ -1,13 +1,13 @@
 # import xmlrpc bagian client
-
+import xmlrpc.client
 
 # buat stub proxy client
-
+proxy = xmlrpc.client.ServerProxy("http://localhost:9999/")
 
 # buka file yang akan diupload
 with open("file_diupload.txt",'rb') as handle:
     # baca file dan ubah menjadi biner dengan xmlrpc.client.Binary
-    
+    data = xmlrpc.client.Binary(handle.read())
 
 # panggil fungsi untuk upload yang ada di server
-
+proxy.file_upload(data)
--- Tugas2_RPC/04.upload_rpc_server.py
+++ Tugas2_RPC/04.upload_rpc_server.py
@@ -1,5 +1,5 @@
 # import SimpleXMLRPCServer bagian server
-
+from xmlrpc.server import SimpleXMLRPCServer
 
 # buat fungsi bernama file_upload()
 def file_upload(filedata):
@@ -10,19 +10,21 @@ def file_upload(filedata):
         data1=filedata.data
         
         # tulis file tersebut
-        
+        handle.write(data1)
         return True  #IMPORTANT
         
 # must have return value
 # else error messsage: "cannot marshal None unless allow_none is enabled"
 
 # buat server
-
+port = 9999
+server = SimpleXMLRPCServer(('',port))
 
 # tulis pesan server telah berjalan
 print ("Listening on port 9999")
 
 # register fungsi 
-
+server.register_function(file_upload, 'file_upload')
 
 # jalankan server
+server.serve_forever()
