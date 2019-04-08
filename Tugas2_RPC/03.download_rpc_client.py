3150e7f2d86c3571cadaea4a1bc4652386e3eb8a
HAFIDZIL <hafidzilkhairi@365.telkomuniversity.ac.id>
2019-04-08 23:26:22 +0700

    have finished number 3 of second task

--- Tugas2_RPC/03.download_rpc_client.py
+++ Tugas2_RPC/03.download_rpc_client.py
@@ -1,13 +1,14 @@
 # import xmlrpc bagian client
-
+import xmlrpc.client
 
 # buat proxy untuk mengakses server. Gunakan parameter URL server yang akan diakses berupa IP dan port. Bentuk http://IP:port
-
+s = xmlrpc.client.ServerProxy('http://192.168.43.122:45678')
 
 # buat/buka file baru dengan nama "hasil_download.txt" sebagai hasil download dari server
-with open("hasil_download.txt","wb") as handle:
-    
+with open("hasil_didownload.txt", "wb") as handle:
+    xmldata = s.file_download()
+
     # tulis/isi file hasil_download.jpg dengan hasil dari memanggil fungsi "download" yang berada server
-    # ubah file menjadi binary dengan menambahkan .data
-    
-    
+    handle.write(xmldata.data)
+
+# ubah file menjadi binary dengan menambahkan .data
--- Tugas2_RPC/03.download_rpc_server.py
+++ Tugas2_RPC/03.download_rpc_server.py
@@ -1,26 +1,24 @@
 # import library SimpleXMLRPCServer
-
-
+from xmlrpc.server import SimpleXMLRPCServer
 # import xmlrpc bagian client
-
+import xmlrpc.client
 
 
 # buatlah fungsi bernama download()
 def file_download():
-
-    # buka file bernama "file_didownload.txt"
-    with open("file_didownload.txt",'rb') as handle:
+    # buka file bernama "phs.jpg"
+    with open("file_didownload.txt", 'rb') as handle:
         # kirimkan file tersebut dalam bentuk xml dengan cara memanggil xmlrpc.client.Binary()
-        
-        
+        binary_data = xmlrpc.client.Binary(handle.read())
+        return binary_data
+
 
 # buat server pada IP dan port yang telah ditentukan
+with SimpleXMLRPCServer(("localhost", 45678)) as a:
+    a.register_introspection_functions()
 
+    # register fungsi download pada server
+    a.register_function(file_download, 'file_download')
 
-# print bahwa "server mendengarkan pada port xxx"
-print ("Listening on port 8001")
-
-# register fungsi download pada server
-
-
-# jalankan server
+    # jalankan server
+    a.serve_forever()
