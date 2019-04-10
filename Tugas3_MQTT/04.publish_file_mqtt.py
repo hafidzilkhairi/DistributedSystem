93402ac326cdf36fb6393db11087b77980cd9def
madityaar <m.adityarayhan@yahoo.co.id>
2019-04-10 16:08:53 +0700

    soal4

--- Tugas3_MQTT/04.publish_file_mqtt.py
+++ Tugas3_MQTT/04.publish_file_mqtt.py
@@ -1,34 +1,43 @@
-# import paho
+####yang dirunning harus file subscribe terlebih dahulu
 
+# import paho
+import paho.mqtt.client as paho
 
 # definsi broker yang digunakan
+broker="localhost"
+filename="surf.jpg"
 
+def on_publish(mosq, obj, mid):
+    print("mid: " + str(mid))
 
 # buat client bernama P1
 print("creating new instance")
-
+P1= paho.Client("P1")
 
 # client terkoneksi ke broker
 print("connecting to broker")
-
-
+P1.connect(broker, 12345,60)
+P1.loop_start()
+    
 # print "baca file"
 print ("read file")
 
 # buka file surf.jpg
-
+f = open(filename, "rb")
 
 # baca semua isi file
-
+imagestring = f.read()
 
 # ubah file dalam bentuk byte gunakan fungsi byte()
-
+byteArray = bytes(imagestring)
 
 # publish dengan topik photo dan data dipublish adalah file
 print("publish foto")
-
+P1.on_publish=on_publish
+P1.publish("photo",byteArray,0)
 
 # client loop mulai
-
+P1.loop_forever()
 
 # tutup file
+f.close
--- Tugas3_MQTT/04.subscribe_file_mqtt.py
+++ Tugas3_MQTT/04.subscribe_file_mqtt.py
@@ -1,34 +1,49 @@
-# gunakan library paho
+####yang dirunning harus file subscribe terlebih dahulu
 
+# gunakan library paho
+import paho.mqtt.client as paho
 
 # gunakan library time
-
+import time
 
 # buat callback pada saat ada pesan masuk
 ###########################################
 def on_message(client, userdata, message):
     # tulis hasil file yang didapat bernama "iris.jpg"
-    
-##########################################
+    print('berhasil diterima')
+    with open('iris.jpg', 'wb') as fd:
+        fd.write(message.payload)
         
+def on_connect(client, userdata, flags, rc):
+    print("OK Connected with result code "+str(rc))
+    client.subscribe(SUBTOPIC, qos=0)
+    print("Subscribe: " + SUBTOPIC)
+
+
+def on_subscribe(client, userdata, mid, gqos):
+    print("subscribed: " + gqos)
+    pass
+##########################################
+
 # definisikan broker yang akan digunakan
-
-
+broker="localhost"
+SUBTOPIC="photo"
 # buat client P2 
 print("creating new instance")
-
+client= paho.Client("P2")
 
 # koneksi P2 ke broker
 print("connecting to broker")
-
+client.connect(broker,12345,61)
 
 # P2 subcribe ke topik "photo"
 print("Subscribing to topic","photo")
-
+client.subscribe("photo", 0)
 
 # callback diaktifkan
-
-
+client.on_message = on_message
+client.on_connect = on_connect
+client.on_subscribe = on_subscribe
 #client.loop_forever()
 while True:
     client.loop(15)
