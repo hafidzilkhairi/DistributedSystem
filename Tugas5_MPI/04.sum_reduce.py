ad68cdd34790ebe60f931f5f31b8f35663e6ed41
madityaar <m.adityarayhan@yahoo.co.id>
2019-04-22 18:22:38 +0700

    kerjaan shidqi tapi sama adit beres

--- Tugas5_MPI/04.sum_reduce.py
+++ Tugas5_MPI/04.sum_reduce.py
@@ -1,24 +1,25 @@
 # import mpi4py
-
+from mpi4py import MPI
+import numpy as np
 
 # import library random untuk generate angka integer secara random
 
 
 # buat COMM
-
+comm = MPI.COMM_WORLD
 
 # dapatkan rank proses
-
+rank=comm.Get_rank()
 
 # dapatkan total proses berjalan
-
+size=comm.Get_size()
 
 # generate angka integer secara random untuk setiap proses
-
+random_val = np.random.randint(100)
 
 # lakukam penjumlahan dengan teknik reduce, root reduce adalah proses dengan rank 0
-
+sum = comm.reduce(random_val, op=MPI.SUM, root=0)
 
 # jika saya proses dengan rank 0 maka saya akan menampilkan hasilnya
-if rank==:
-
+if rank==0:
+    print("Hasil Penjumlahan ",sum)
