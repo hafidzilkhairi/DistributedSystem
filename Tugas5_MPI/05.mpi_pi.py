4940709323cc9607afdf77a3fb3dc6f5fca6232e
anonymous <anonymous@localhost.localdomain>
2019-04-22 20:47:07 +0700

    have Finished number 5 of 5th task

--- Tugas5_MPI/05.mpi_pi.py
+++ Tugas5_MPI/05.mpi_pi.py
@@ -1,5 +1,5 @@
-# import mpi4py
-
+from mpi4py import MPI
+import math, time
 
 # buat fungsi dekomposisi bernama local_loop 
 # local_loop akan menghitung setiap bagiannya
@@ -10,38 +10,40 @@ def local_loop(num_steps,begin,end):
     sum = 0
     # 4/(1+x^2)
     for i in range(begin,end):
-        
-    print (sum)
+        x = (i+0.5)*step
+        sum += 4.0/(1.0+x*x)
     return sum    
 
 # fungsi Pi
 def Pi(num_steps):
-    
     # buat COMM
-    
+    comm = MPI.COMM_WORLD
     
     # dapatkan rank proses
-    
+    rank = comm.Get_rank()
     
     # dapatkan total proses berjalan
-    
+    size = comm.Get_size()
     
     # buat variabel baru yang merupakan num_steps/total proses
-    
+    baru = num_steps/size
     
     # cari local_sum
     # local_sum merupakan hasil dari memanggil fungsi local_loop
-    
-    
+    local_sum = local_loop(num_steps,rank*num_steps/size, rank*num_steps/size+num_steps/size)
     # lakukan penjumlahan dari local_sum proses-proses yang ada ke proses 0
     # bisa digunakan reduce atau p2p sum
-    
-    
+#     print rank,' have data: ',local_sum
+    sum = 0
+    sum = comm.reduce( local_sum, op=MPI.SUM, root=0)
     # jika saya proses dengan rank 0  maka tampilkan hasilnya
     if rank == 0:
         pi = sum / num_steps
         print (pi)
+
     
 # panggil fungsi utama    
 if __name__ == '__main__':
+    # awal = time.time()
     Pi(10000)
+    # print(time.time()-awal)
