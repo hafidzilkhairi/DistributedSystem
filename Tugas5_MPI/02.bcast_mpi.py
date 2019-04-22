9f5ccc24ad2da3d549a34b7d0601dbf0b36e7978
shidqiaqil <shidqiaqil@users.noreply.github.com>
2019-04-22 12:08:58 +0700

    no 2

--- Tugas5_MPI/02.bcast_mpi.py
+++ Tugas5_MPI/02.bcast_mpi.py
@@ -1,19 +1,19 @@
 # import mpi4py
-
+from mpi4py import MPI
 
 # buat COMM
-
+comm = MPI.COMM_WORLD
 
 # dapatkan rank proses
-
+rank = comm.Get_rank()
 
 # dapatkan total proses berjalan
-
+size = comm.Get_size()
 
 # jika saya rank 0 maka saya akan melakukan broadscast
-if rank == :
-
+if rank == 0:
+   comm.bcast("HALO POLISI", root=0)
 	
 # jika saya bukan rank 0 maka saya menerima pesan
 else:
-	
+    print ("rank ",rank,comm.bcast(""))
